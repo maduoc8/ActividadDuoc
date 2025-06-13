200ff76576bb1bd1728eb5dbd02919690c9c3317
FreLuna23 <fre.luna@duocuc.cl>
2025-06-13 19:46:40 -0400

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,15 @@
+cliente = {"rutes",
+           "nombres",
+           "apellidospaternos",
+           "edades",
+           "estadosciviles",
+           "generos",
+        "fechasdeafiliaciones"}
+
+rut = input("Ingrese su rut: ")
+nombre = input("Ingrese su nombre: ")
+apellido_paterno = input("Ingrese su apellido paterno: ")
+edad = int(input)("Ingrese su edad: ")
+estado_civil = str(input("Ingrese el estado civil: (c = casado), (s = soltero), (v = viudo)"))
+genero = input("Ingrese su genero: ")
+fecha_de_afiliacion = int(input("Ingrese la fecha de afiliación: "))
