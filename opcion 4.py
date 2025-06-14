192127f5dada96111f92207ece21abe218a18e97
maduoc8 <ma.simon@duocuc.cl>
2025-06-13 20:32:41 -0400

    Add files via upload

---
+++ opcion 4.py
@@ -0,0 +1,16 @@
+from random import randint
+
+opcion = 4
+numeroaleatorio = randint(1000,1500)
+
+if opcion == 4:
+    print(f"nombre: {nombre}")
+    print(f"apellido paterno : {}")
+    print(f"rut": {rut})
+    print(f"edad" {edad}"")
+    print(f"estado civil: {estado civil}")
+    print(f"genero {genero}")
+    print(f"fecha de afiliacion {fecha de afiliacion}")
+
+    print(f"el valor es {numeroaleatorio}")
+    print(f"")
